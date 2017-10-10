7d786ae50b32c106fc0675bf7bf2c630a9c0e86d
Peter Harrington <pharrington@g.harvard.edu>
2017-10-10 11:38:10 -0400

    code for making api requests created

--- data/load_quandl_sf1.py
+++ data/load_quandl_sf1.py
@@ -7,7 +7,9 @@ import quandl
 from os import environ
 import sys
 
+DS_NAME = "SF1"
 
+# TODO: move this function to utils/
 def set_api_key():
     """read QUANDL_API_KEY env variable, and set it."""
     try:
@@ -25,8 +27,25 @@ if __name__ == '__main__':
     # returns Pandas DataFrame with Date as index and Value
     #jj = quandl.get('SF1/WMT_GP_MRQ', start_date='2017-07-31', end_date='2017-07-31')
 
-    jj = quandl.get('SF1/WMT_GP_MRQ')
-    print(type(jj))
-    print(jj)
+    tickers = ["WMT"]
+    fields = ["GP", "CAPEX", "EBIT", "ASSETS"]
+    at_time_of = False  # if this is true append "", else append "_MRQ"
+
+    if at_time_of:
+        suffix = ""
+    else:
+        suffix = "MRQ"
+    # for every ticker in ticker_list get
+    fields_a = map(lambda s:"%s_%s" % (s, suffix), fields)
+
+    for ticker in tickers:
+        for field in fields_a:
+            query_str =  "%s/%s_%s" % (DS_NAME, ticker, field)
+            df = quandl.get(query_str)
+    #   fetch each field in fields
+    #   join data of all fields and write to file: raw/
+
+    #print(type(jj))
+    #print(jj)
 
     print("this worked boss")
