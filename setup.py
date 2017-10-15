e5e9f0529000e12b2c37129ee09ede226ce0eaea
Peter Harrington <pharrington@g.harvard.edu>
2017-10-15 13:05:22 -0400

    setup.py first draft

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Alpha Compiler',
+      version='0.1',
+      description='Python tools for quantitative finance',
+      author='Peter Harrington',
+      author_email='peter.b.harrington@gmail.com',
+      url='https://www.alphacompiler.com/',
+      packages=['alphacompiler', 'alphacompiler.util', 'alphacompiler.data'],
+     )
