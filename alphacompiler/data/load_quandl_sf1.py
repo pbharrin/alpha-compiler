204e09582cd93fb8fdaa3d935e1526dc113d8ea2
Peter Harrington <pharrington@g.harvard.edu>
2020-05-27 10:23:37 -0700

    SF1 loader now uses field_dimension like sps_ART

--- alphacompiler/data/load_quandl_sf1.py
+++ alphacompiler/data/load_quandl_sf1.py
@@ -16,6 +16,7 @@ from logbook import Logger
 import datetime
 import os
 import pandas as pd
+import glob
 
 BASE = os.path.dirname(os.path.realpath(__file__))
 DS_NAME = 'SHARADAR/SF1'   # quandl DataSet code
@@ -30,6 +31,13 @@ DUMP_FILE = '/Users/peterharrington/Downloads/SHARADAR_SF1_2daa4baaad2a300c166b5
 
 log = Logger('load_quandl_sf1.py')
 
+
+def clear_raw_folder(raw_folder_path):
+    # removes all the files in the raw folder
+    print('   **   clearing the raw/ folder   **')
+    files = glob.glob(raw_folder_path + '/*')
+    for f in files:
+        os.remove(f)
 
 
 def populate_raw_data_from_dump(tickers2sid, fields, dimensions, raw_path):
@@ -46,7 +54,11 @@ def populate_raw_data_from_dump(tickers2sid, fields, dimensions, raw_path):
 
     df = pd.read_csv(DUMP_FILE)  # open dump file
 
+    clear_raw_folder(RAW_FLDR)
+
     df = df[['ticker', 'dimension', 'datekey'] + fields]  # remove columns not in fields
+    df = df.loc[:, ~df.columns.duplicated()]  # drop any columns with redundant names
+
     for tkr, df_tkr in df.groupby('ticker'):
         print('processing: ', tkr)
 
@@ -54,15 +66,20 @@ def populate_raw_data_from_dump(tickers2sid, fields, dimensions, raw_path):
         if sid is None:
             print('no sid found for: {}'.format(tkr))
             continue
+        print('sid: {}'.format(sid))
 
         df_tkr = df_tkr.rename(columns={'datekey': 'Date'}).set_index('Date')
 
         # loop over the fields and dimensions
         series = []
         for i, field in enumerate(fields):
+            print(i, field)
             s = df_tkr[df_tkr.dimension == dimensions[i]][field]
+            new_name = '{}_{}'.format(field, dimensions[i])
+            s = s.rename(new_name)
             series.append(s)
         df_tkr = pd.concat(series, axis=1)
+        df_tkr.index.names = ['Date']  # ensure that the index is named Date
         print("AFTER reorganizing")
         print(df_tkr)
 
@@ -176,29 +193,36 @@ if __name__ == '__main__':
     # dimensions1 = ['ART', 'ARQ', 'ARQ', 'ARQ', 'ARQ', 'ARQ', 'ARQ']
 
     # Marc's turntup Quality companies in an uptrend
-    # fields2 = ['roe', 'marketcap', 'de', 'debt', 'debtnc']
-    # dimensions2 = ['ART', 'ARQ', 'ARQ', 'ARQ', 'ARQ']
+    fields2 = ['roe', 'marketcap', 'de', 'debt', 'debtnc']
+    dimensions2 = ['ART', 'ARQ', 'ARQ', 'ARQ', 'ARQ']
 
     # more value
     # fields3 = ['ebitda', 'ev', 'pe', 'pe1', 'marketcap']
     # dimensions3 = ['ARQ', 'ARQ', 'ARQ', 'ARQ', 'ARQ']
-    #
-    # fields = fields0 + fields3
-    # dimensions = dimensions0 + dimensions3
 
-    fields = ['assetsavg','bvps','capex','cashnequsd','debtusd','dps','ebitdausd','ebitusd','equityavg','equityusd',
-               'fcf','fcfps', 'gp', 'intangibles','liabilitiesc','ncfo','netinc','netinccmn','netinccmnusd','price',
-               'revenueusd','roa','roe','roic','sharefactor','sps','sharesbas','shareswa']
-    dimensions = ['ART'] * len(fields)  # use all ART
+    fields4 = ['netinc', 'equity', 'bvps', 'sps', 'fcfps', 'price', 'roe', 'roe']
+    fields = fields4
+    dimensions = ['ART', 'ART', 'ART', 'ART', 'ART', 'ART', 'ART', 'ARQ']
+
+    # fields = ['assetsavg','bvps','capex','cashnequsd','debtusd','dps','ebitdausd','ebitusd','equityavg','equityusd',
+    #            'fcf','fcfps', 'gp', 'intangibles','liabilitiesc','ncfo','netinc','netinccmn','netinccmnusd','price',
+    #            'revenueusd','roa','roe','roic','sharefactor','sps','sharesbas','shareswa']
+    # fields = ['assetsavg', 'bvps', 'capex', 'cashnequsd', 'debtusd', 'dps', 'ebitdausd', 'ebitusd', 'equityavg',
+    #            'equityusd',
+    #            'ev', 'fcf', 'fcfps', 'gp', 'intangibles', 'intexp', 'liabilities', 'liabilitiesc', 'ncfo', 'netinc',
+    #            'netinccmn', 'netinccmnusd', 'price', 'revenueusd', 'roa', 'roe', 'roic', 'sharefactor', 'sps',
+    #            'sharesbas', 'shareswa']
+    # dimensions = ['ART'] * len(fields)  # use all ART
 
     BUNDLE_NAME = 'sep'
     num_tickers = num_tkrs_in_bundle(BUNDLE_NAME)
     print('number of tickers: ', num_tickers)
 
     all_tickers_for_bundle_from_dump(fields, dimensions, 'sep')  # downloads the data to /raw
+    fields_dimensions = ['{}_{}'.format(i, j) for i, j in zip(fields, dimensions)]
     pack_sparse_data(num_tickers + 1,  # number of tickers in buldle + 1
                     os.path.join(BASE, RAW_FLDR),
-                    fields,
+                    fields_dimensions,
                     ZIPLINE_DATA_DIR + FN)  # write directly to the zipline data dir
 
 
